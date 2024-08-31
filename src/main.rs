b33a53689f3d2384511c254cb866ed988198d02e
Andrew Lucas <lucas.andrew.j@proton.me>
2024-08-31 11:44:39 -0700

    Implemented handling of UTC times

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use std::env;
 use std::fs::File;
 use std::io::BufReader;
 use std::io::prelude::*;
-use time::OffsetDateTime;
+use time::{Date, OffsetDateTime, Time, UtcOffset};
 use time::Month;
 
 struct RRule {
@@ -26,7 +26,56 @@ fn main() {
 }
 
 fn offset_date_time_from_ical(ical_str: &str) -> OffsetDateTime {
-    todo!()
+    let mut start = 0;
+    let mut local_time = false;
+
+    if ical_str[0..4].eq("TZID") {
+        local_time = true;
+        for c in ical_str.chars() {
+            start += 1;
+            if c == ':' {
+                break;
+            }
+        }
+    } else if ical_str.chars().nth(15) != Some('Z') {
+        panic!("Invalid date time format.");
+    }
+
+    let year = ical_str[start..start + 4].parse::<i32>().expect("Invalid date time format");
+    let month = ical_str[start + 4..start + 6].parse::<u8>().expect("Invalid date time format");
+    let month = match month {
+        1 => Month::January,
+        2 => Month::February,
+        3 => Month::March,
+        4 => Month::April,
+        5 => Month::May,
+        6 => Month::June,
+        7 => Month::July,
+        8 => Month::August,
+        9 => Month::September,
+        10 => Month::October,
+        11 => Month::November,
+        12 => Month::December,
+        _ => panic!("Invalid month value")
+    };
+    let day = ical_str[start + 6..start + 8].parse::<u8>().expect("Invalid date time format");
+
+    let hour = ical_str[start + 9..start + 11].parse::<u8>().expect("Invalid date time format");
+    let minute = ical_str[start + 11..start + 13].parse::<u8>().expect("Invalid date time format");
+    let second = ical_str[start + 13..start + 15].parse::<u8>().expect("Invalid date time format");
+
+    let date = Date::from_calendar_date(year, month, day).expect("Invalid date values");
+    let time = Time::from_hms(hour, minute, second).expect("Invalid time values");
+
+    let offset =
+    if local_time {
+        UtcOffset::from_hms(0, 0, 0).expect("Error processing offset")
+        //TODO Get the TZID and find the matching offset to go with it. Use that.
+    } else {
+        UtcOffset::from_hms(0, 0, 0).expect("Error processing offset")
+    };
+
+    OffsetDateTime::new_in_offset(date, time, offset)
 }
 
 #[cfg(test)]
@@ -45,6 +94,7 @@ mod tests {
         assert!(dt.second() == 00);
     }
 
+    #[test]
     fn ical_datetime_utc_to_pdt() {
         let dt = offset_date_time_from_ical("20240310T120000Z");
 
