ae9199c0d79071143e9f21cc9c1efb942e05bdae
Andrew <lucas.andrew.j@proton.me>
2025-02-12 21:34:04 -0800

    Adds data structures for recurrence rules

--- src/components.rs
+++ src/components.rs
@@ -30,6 +30,50 @@ enum EventEnd {
     Duration(Time),
 }
 
-struct Rule {
-    // TODO
+// Using explicit values for properties because it is probably smaller than a BTreeMap, and
+// definitely faster to access. Can look into the size comparison later.
+struct RRule {
+    freq: Freq,
+    recur_end: Option<RecurEnd> ,
+    interval: Option<>,
+    bysecond: Option<usize>,
+    byminute: Option<usize>,
+    byhour: Option<usize>,
+    byday: Option<List<(Weekday, Option<isize>>>,
+    bymonthday: Option<isize>,
+    byyearday: Option<isize>,
+    byweekno: Option<isize>,
+    bymonth: Option<usize>,
+    bysetpos: Option<isize>,
+    wkst: Option<Weekday>,
+}
+
+enum Freq {
+    SECONDLY,
+    MINUTELY,
+    HOURLY,
+    DAILY,
+    WEEKLY,
+    MONTHLY,
+    YEARLY,
+}
+
+enum RecurEnd {
+    Until(DateOrDateTime),
+    Count(usize),
+}
+
+enum Weekday {
+    SU,
+    MO,
+    TU,
+    WE,
+    TH,
+    FR,
+    SA,
+}
+
+enum DateOrDateTime {
+    Date(Date),
+    DateTime(OffsetDateTime),
 }
