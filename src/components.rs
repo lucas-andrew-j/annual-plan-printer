2ac91878dc3172a061a12677eea669a36f5e725b
Andrew <lucas.andrew.j@proton.me>
2025-03-23 01:26:00 -0700

    Adds RRuleBuilder

--- src/components.rs
+++ src/components.rs
@@ -36,8 +36,8 @@ pub struct Event {
 // definitely faster to access. Can look into the size comparison later.
 pub struct RRule {
     freq: Freq,
-    until: DateOrDateTime,
-    count: usize,
+    until: Option<DateOrDateTime>,
+    count: Option<usize>,
     interval: usize,
     by_second: Option<Vec<usize>>,
     by_minute: Option<Vec<usize>>,
@@ -64,4 +64,50 @@ enum Freq {
 enum DateOrDateTime {
     Date(Date),
     DateTime(OffsetDateTime),
+}
+
+pub struct RRuleBuilder {
+    freq: Option<Freq>,
+    until: Option<DateOrDateTime>,
+    count: Option<usize>,
+    interval: usize,
+    by_second: Option<Vec<usize>>,
+    by_minute: Option<Vec<usize>>,
+    by_hour: Option<Vec<usize>>,
+    by_day: Option<Vec<(Weekday, Option<isize>)>>,
+    by_month_day: Option<Vec<isize>>,
+    by_year_day: Option<Vec<isize>>,
+    by_week_no: Option<Vec<isize>>,
+    by_month: Option<Vec<usize>>,
+    by_set_pos: Option<Vec<isize>>,
+    week_start: Weekday,
+}
+
+impl RRuleBuilder {
+    fn new() -> RRuleBuilder {
+        RRuleBuilder {
+            freq: None,
+            until: None,
+            count: None,
+            interval: 1,
+            by_second: None,
+            by_minute: None,
+            by_hour: None,
+            by_day: None,
+            by_month_day: None,
+            by_year_day: None,
+            by_week_no: None,
+            by_month: None,
+            by_set_pos: None,
+            week_start: Weekday::Monday,
+        }
+    }
+
+    fn set_freq(&mut self, freq: Freq) -> &mut RRuleBuilder {
+        todo!();
+    }
+
+    fn build(self) -> RRule {
+        todo!();
+    }
 }
